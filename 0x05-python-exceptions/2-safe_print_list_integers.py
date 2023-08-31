446b6cf32047d45b222f325e2462fc48569c461b
luckys-lnx <lalucky22@gmail.com>
2023-08-31 08:12:40 -0700

    2. Print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    nb_print = 0
+    """variable to store the x element"""
+    first_elem = 0
+
+    """iterate over the x element to collect the fiestElem"""
     for i in range(x):
+
+        """print all integers on the same line,skip other values"""
         try:
+
+            """using try to format integers to be printed from my_list"""
             print("{:d}".format(my_list[i]), end="")
-            nb_print += 1
+
+            """incrementing first_elem, to get next Val"""
+            first_elem +=1
+
+            """throw an exception if x > my_list"""
         except (ValueError, TypeError):
+
+            """skip other values not integers"""
             pass
+
+        """print new line"""
     print()
-    return (nb_print)
+
+    """return elements x"""
+    return (first_elem)
