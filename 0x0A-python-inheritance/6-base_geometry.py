c8af53a6b284a39efbacf09eeaeaca59d7735c0b
luckys-lnx <lalucky22@gmail.com>
2023-09-11 16:01:47 -0700

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""an empty class BaseGeometry"""
+class BaseGeometry:
+    """ A class with public attribute area() """
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
