302b011d3ad62d949336ac3f35e30d1abf39ad4c
luckys-lnx <lalucky22@gmail.com>
2023-09-11 14:00:51 -0700

    0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """
+        lookup: check for available list and attributes of obj
+
+        Returns: A list of attributes and obj
+    """
     return (dir(obj))
