413c3fa65526b65ce0e43b89cdf5cc5ef8257433
luckys-lnz <lalucky22@gmail.com>
2024-05-22 12:38:07 -0700

    convert to utf-8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,4 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SELECT CONVERT(binary CONVERT(name USING latin1) USING utf8mb4) FROM first_table;
