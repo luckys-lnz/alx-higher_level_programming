6abac7f59375a752445244497cf9b8314970ab48
luckys-lnz <lalucky22@gmail.com>
2024-05-24 12:45:31 +0000

    added ORDER BY cities ASC;

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,5 @@
 -- can be found in the database hbtn_0d_usa.
 
 SELECT cities.id, cities.name FROM cities
-WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California');
+WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
