ac233c498bb12123bdf378bd08edaf80da2663cf
luckys-lnz <lalucky22@gmail.com>
2024-06-19 22:47:15 +0000

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,8 +4,8 @@ const arg = process.argv[2];
 const x = parseInt(arg);
 
 if (isNaN(x)) {
-  console.log('Missing size');
+  console.log("Missing size");
 }
 for (let i = 0; i < x; i++) {
-  console.log('x'.repeat(x));
+  console.log("x".repeat(x));
 }
