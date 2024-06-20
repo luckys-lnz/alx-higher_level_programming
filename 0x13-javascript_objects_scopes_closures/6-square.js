6cf24a4f49a976aba6ba01466570fadfb34863ab
luckys-lnz <lalucky22@gmail.com>
2024-06-20 21:46:27 +0000

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  charPrint (c) {
+    const char = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
