9cc62699ff07a50a2ce6182d9d90d54e0f8a7f6f
luckys-lnz <lalucky22@gmail.com>
2024-06-20 21:47:49 +0000

    3. Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // If w or h is 0, negative, or not an integer, create an empty object
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    // check if width and height exist
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
