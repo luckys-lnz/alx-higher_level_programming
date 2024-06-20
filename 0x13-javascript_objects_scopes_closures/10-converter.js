d9a107092b0d5ddbf1b45ae530d64f3094f615fb
luckys-lnz <lalucky22@gmail.com>
2024-06-20 23:41:34 +0000

    10. Number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,20 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-  return function convert (number) {
-    if (number === 0) {
-      return '0';
-    }
-
-    let result = '';
-
-    while (number > 0) {
-      result = digits[number % base] + result;
-      number = Math.floor(number / base);
-    }
-
-    return result;
+  return function (number) {
+    return number.toString(base);
   };
 };
