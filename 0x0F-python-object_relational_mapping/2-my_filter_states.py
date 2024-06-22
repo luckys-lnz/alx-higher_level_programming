96bc9d5cc1f33130cdbceb58d018b26395f033b1
luckys-lnz <lalucky22@gmail.com>
2024-06-22 09:24:51 -0700

    2. Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-script that takes in an argument and 
-displays all values in the states table 
+script that takes in an argument and
+displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument.
 
 script takes 4 arguments.
@@ -23,17 +23,15 @@ if __name__=='__main__':
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3])
-    
+
     # create cursor object - execute queries
     cur = connector.cursor()
 
-    # execute sql query
-    query = """
-             SELECT *
-             FROM states
-             WHERE BINARY name = %s
-             ORDER BY id ASC
-             """
+    # sql query with arguments string formater
+    query=("""SELECT * FROM states WHERE
+            BINARY name = %s ORDER BY id ASC""")
+
+    #execute query using format
     cur.execute(query, (stateName,))
 
     # fetch results
