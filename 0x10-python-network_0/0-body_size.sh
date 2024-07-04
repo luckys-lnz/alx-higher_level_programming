a823dd50fa690a9d05b5a06d180fb421b37fd5d8
luckys-lnz <lalucky22@gmail.com>
2024-07-04 15:47:22 -0700

    modified

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
+# Get the byte size of the HTTP response body for a given URL.
 curl -s "$1" | wc -c
+
