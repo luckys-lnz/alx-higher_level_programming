ba1ff287aa3fd0e24be33212618501f580ae452f
luckys-lnz <lalucky22@gmail.com>
2024-07-04 15:49:30 -0700

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# URL from the command line argument
-URL="$1"
-
-# Send request to the URL and get the size of the body in bytes
-SIZE=$(curl -s -o /dev/null -w '%{size_download}' "$URL")
-
-# Display the size
-echo "$SIZE"
-
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
